2b6616c2a1eb6d5ba2611a3e431371d340f358f1
yerboygiles <43770337+yerboygiles@users.noreply.github.com>
2019-11-22 16:36:20 -0800

    Add files via upload

---
+++ practice33.m
@@ -0,0 +1,38 @@
+%% practice VIP
+
+clear all; close all;
+
+%%imth = [zeros(2,8);zeros(5,2),[0;ones(3,1);0],ones(5,2),[ones(4,1);0],[0,ones(2,1);zeros(2,1)],zeros(5,1);zeros(1,8);]
+
+l = 255;
+d = 000;
+imth = [
+    d d d d d d d d; 
+    d d d d d d d d; 
+    d d d l l l d d;
+    d d l l l l l d;
+    d d l l l l l d;
+    d d l l l l d d;
+    d d d l l d d d;
+    d d d d d d d d;
+    ]
+
+
+
+
+
+
+
+
+
+
+
+%nimth = [56]
+%for i=1:1:56;
+%    if(i>56);
+%        nimth(i)=imth(i-64);
+%    else;
+%        nimth(i)=imth(i+8);
+%    end
+%end
+%snimth=reshape(nimth,[8,8])
